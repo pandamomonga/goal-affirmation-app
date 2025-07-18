ab1bf8d2014e918ccd76beaf49f0481f2e93fd81
pandamomonga <134188447+pandamomonga@users.noreply.github.com>
2025-07-18 19:51:51 +0900

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>目標達成アファメーション生成アプリ</h1>
+      <p>シンプルバージョン</p>
+    </div>
+  );
+}
